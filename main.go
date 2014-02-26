7ac86a18b5b6a7bbcf2b6da12c723c6da3656c91
Andrew Gerrand <adg@golang.org>
2014-02-27 07:33:58 +1100

    automatically open browser

--- main.go
+++ main.go
@@ -18,9 +18,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/nf/sigourney/ui"
 
@@ -32,6 +37,7 @@ import (
 var (
 	listenAddr = flag.String("listen", "localhost:8080", "listen address")
 	doDemo     = flag.Bool("demo", false, "play demo sound")
+	doBrowser  = flag.Bool("browser", true, "open web browser")
 )
 
 func main() {
@@ -52,13 +58,19 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	http.HandleFunc("/socket", socketHandler)
-	go func() {
-		if err := http.ListenAndServe(*listenAddr, nil); err != nil {
-			log.Println(err)
-		}
-	}()
 
-	os.Stdout.Write([]byte("Press enter to stop...\n"))
+	l, err := net.Listen("tcp", *listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go http.Serve(l, nil)
+
+	u := fmt.Sprintf("http://%v/", *listenAddr)
+	if !*doBrowser || !openBrowser(u) {
+		fmt.Printf("Open your web browser to %v\n\n", u)
+	}
+
+	fmt.Println("Press enter to quit...")
 	os.Stdin.Read([]byte{0})
 }
 
@@ -79,7 +91,9 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 	go func() {
 		for m := range u.M {
 			if err := c.WriteJSON(m); err != nil {
-				log.Println(err)
+				if err != io.EOF {
+					log.Println(err)
+				}
 				return
 			}
 		}
@@ -88,11 +102,29 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 	for {
 		m := new(ui.Message)
 		if err := c.ReadJSON(m); err != nil {
-			log.Println(err)
+			if err != io.EOF {
+				log.Println(err)
+			}
 			return
 		}
 		if err := u.Handle(m); err != nil {
 			log.Println(err)
 		}
 	}
+}
+
+// openBrowser tries to open the URL in a browser,
+// and returns whether it succeed in doing so.
+func openBrowser(url string) bool {
+	var args []string
+	switch runtime.GOOS {
+	case "darwin":
+		args = []string{"open"}
+	case "windows":
+		args = []string{"cmd", "/c", "start"}
+	default:
+		args = []string{"xdg-open"}
+	}
+	cmd := exec.Command(args[0], append(args[1:], url)...)
+	return cmd.Start() == nil
 }
