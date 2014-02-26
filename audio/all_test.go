bbc158f73d7a8a5471ee0b4d495d4e54fbf0bd55
Andrew Gerrand <adg@golang.org>
2014-02-27 08:56:06 +1100

    move delay test to all_test

--- audio/all_test.go
+++ audio/all_test.go
@@ -40,3 +40,23 @@ func BenchmarkFMSin(b *testing.B) {
 		o.Process(buf)
 	}
 }
+
+func TestDelay(t *testing.T) {
+	sum := NewSum()
+	sum.Input("a", Value(1))
+	dly := NewDelay()
+	dly.Input("in", sum)
+	dly.Input("len", Value(Sample(nSamples)/waveHz))
+	dup := NewDup(dly)
+	out := dup.Output()
+	sum.Input("b", dup.Output())
+
+	b := make([]Sample, nSamples)
+	for i := 0; i < 6; i++ {
+		dup.Tick()
+		out.Process(b)
+	}
+	if b[0] != 3 {
+		t.Errorf("b[0] == %v, want 3", b[0])
+	}
+}
